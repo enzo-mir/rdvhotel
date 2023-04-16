f7be8c0a5f2a6ca0ea28de4d4b263ac183d4ab26
enzo <miraglioenzo93@gmail.com>
2023-04-16 17:37:04 +0200

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mysql from "mysql";
+const db = mysql.createConnection({
+  host: "enzomrg.com",
+  database: "uqkn2942_enzmrg",
+  user: "uqkn2942_enzmrg",
+  password: "5()Amg9709",
+});
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  db.connect((err) => {
+    if (err) return console.log(err);
+
+    if (!err) {
+      db.query("SELECT * FROM `heures`", (error, heures) => {
+        error ? console.log(error) : null;
+        db.query("SELECT * FROM `reserver`", (error, reservation) => {
+          error ? console.log(error) : null;
+          db.query(
+            "SELECT `id`, `titre`, `description`, `lien` FROM `images`",
+            (error, image) => {
+              error ? console.log(error) : null;
+              res.send({ heures, reservation, image });
+            }
+          );
+        });
+      });
+    }
+  });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       <Head>
@@ -26,7 +33,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -110,5 +117,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
